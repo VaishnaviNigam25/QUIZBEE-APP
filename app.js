0f14e9dd205b35b11d02e103775ce5607f35e772
VAISHNAVI NIGAM <vaishnavinigam25@gmail.com>
2022-01-20 17:29:02 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+var naam=prompt("Enter Your name to begin the Game...")
+
+var checkbtn = document.getElementById('check');
+var response = document.getElementById('response');
+var rightanswer =document.getElementById('rightanswer');
+var nextbtn = document.getElementById('next');
+var firstnum = document.getElementById('multiplier');
+var answer = document.getElementById('ans');
+var secondnum = document.getElementById('multipliend');
+checkbtn.addEventListener("click",checkans);
+nextbtn.addEventListener("click",nextques);
+function checkans(){
+   
+ 
+if(answer.value==(firstnum.innerText*secondnum.innerText))
+{
+   
+    response.innerHTML="Good Job "+ naam +" ✔️";
+    response.style.color ="green";
+}
+else{
+    
+    response.innerHTML="AWAPS ❌ TRY AGAIN..";
+    response.style.color ="red";
+    rightanswer.innerText="Right answer is = "+firstnum.innerText*secondnum.innerText;
+    rightanswer.style.color ="green";
+}
+}
+function nextques(){
+firstnum.innerText = Math.floor((Math.random() * 10) + 1);
+secondnum.innerText = Math.floor((Math.random() * 10) + 1);
+
+response.innerHTML="";
+rightanswer.innerText="";
+answer.innerText = "";   
+}
+
